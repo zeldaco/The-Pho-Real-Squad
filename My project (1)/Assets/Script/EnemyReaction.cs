5d6031e13df37bfcc868f5512461eae7c9f374c6
Zelda <zeldac@hawaii.edu>
2024-03-13 11:37:16 -1000

    updated scripts idk what im doing tho :))

---
+++ My project (1)/Assets/Script/EnemyReaction.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // for restarting level
+
+public class EnemyReaction : MonoBehavior {
+    Rigidbody2D enemyRigidBody2D;
+    public int UnitsToMove = 5;
+    public float EnemySpeed = 500;
+    public bool _isFacingRight;
+    private float _startPos;
+    private float _endPos;
+    public bool _moveRight = true;
+
+    // Use for initialization
+    public void Awake() {
+        enemyRigidBody2D = GetComponent<enemyRigidBody2D>();
+        _startPos = transform.position.x;
+        _endPos = _startPos + UnitsToMove;
+        _isFacingRight = transform.localScale.x > 0;
+    }
+
+    void Start () {
+
+    }
+
+    // Update called once per frame
+    void Update() {
+        if (_moveRight) {
+            enemyRigidBody2D.AddForce(vector2.right * EnemySpeed * Time.deltaTime);
+            if (!_isFacingRight)
+                Flip();
+        }
+        
+        if (enemyRigidBody2D.position.x >= _endPos)
+            _moveRight = false;
+        
+        if (!_moveRight) {
+
+            enemyRigidBody2D.AddForce(-Vector2.right * EnemySpeed * Time.deltaTime);
+            if (_isFacingRight)
+                Flip();
+        }
+
+        if (enemyRigidBody2D.position.x <= _startPos)
+            _moveRight = true;
+
+    }
+
+    public void Flip () {
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        _isFacingRight = transform.localScale.x > 0;
+    }
+
+    // Called when another collider overlaps the trigger collider
+    void OnTriggerEnter2D (Collider2D other) {
+        // If the overlaped collider is an enemy
+        if (other.CompareTag ("Enemy")) {
+            // SCENE HAS TO BE IN BUILD SETTINGS
+            SceneManager.LoadScene("scene1");
+
+        }
+    }
+}
---
+++ My project (1)/Assets/Script/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using UnityEngine:
+using System.Collections;
+
+public class PlayerController : MonoBehavior {
+    // Movement
+    public float speed;
+    public float jump;
+    float moveVelocity;
+    public Rigidbody2D rb;
+    bool isGrounded;
+
+    private Vector2 movement = Vector3.left * 0.1f;
+
+    void Update () {
+
+        //Move camera screen with player
+        if (player.position.x > 1) 
+            movement = vector3.left * 0.1f;
+        else if (player.position.x < -1)
+            movement = Vector3.right * 0.1f;
+        transform.Translate(movement);
+
+        // Grounded?
+        if (isGrounded == true) {
+            // Jumping
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(Update.Arrow) || Input.GetKeyDown(KeyCode.Z) || Input.GetKeyDown(KeyCode.W)){
+                
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
+            }
+        }
+        
+        moveVelocity = 0;
+        //Left Right Movement
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+            moveVelocity = -speed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+            moveVelocity = speed;
+        }
+
+        GetComponent<Rigidbody2D>.velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+
+    void OnCollisionEnter2D(Collision2D col) {
+        Debug.Log("OnCollisionEnter2D");
+        isGrounded = true;
+    }
+    void onCollisionExit2D(Collision2D col) {
+        Debug.Log("OnCollisionExit2D");
+        isGrounded = false;
+    }
+}
