874c22596c03de959fb7bf446a78c19ec61b458d
darriusdacquel <dacqueld@hawaii.edu>
2024-04-11 17:51:02 -1000

    basic healthbar
    if you press E, she takes 1 dmg

---
+++ GroupProject/Assets/Scripts/Health/HealthBar.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Health playerHealth;
+    [SerializeField] private Image totalhealthBar;
+    [SerializeField] private Image currentHealthBar;
+
+    void Start()
+    {
+        totalhealthBar.fillAmount = playerHealth.currentHealth / 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentHealthBar.fillAmount = playerHealth.currentHealth / 10;
+    }
+}
